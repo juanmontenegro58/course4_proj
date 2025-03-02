5d3ac8459fe8c181de60ae221db8f4843af7240d
Juan Lowy <juanmontenegro58@gmail.com>
2025-03-02 16:24:08 -0500

    Finish scheduling with celery

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -42,7 +42,8 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
-        'django_celery_results'
+        'django_celery_results',
+        'django_celery_beat'
     ]
 
     MIDDLEWARE = [
